b66cb8fa078e7a2ed1b566a4ded77b6f7b1a760a
Cory Henderson <30273325+chenderson1@users.noreply.github.com>
2017-12-13 20:59:58 -0800

    comments and fomat

--- getComments.js
+++ getComments.js
@@ -3,41 +3,46 @@ const request = require('request'),
     cheerio = require('cheerio')
 
 module.exports = {
-getThemComments: _getThemComments
+    getThemComments: _getThemComments
 }
 
-// var url = 'https://www.reddit.com/r/warriors/'
+// let url = 'https://www.reddit.com/r/warriors/'
 function _getThemComments(url) {
     request(url, (error, response, html) => {
         if (!error) {
-            var count = 0
-            var currentdate = new Date()
-            var timeDiff
-            var $ = cheerio.load(html)
 
-            var title, submittedDate, submittedTimed
-            var json = []
-            var comment = {}
-            var countIndex = ''
+            let count = 0
+            let currentdate = new Date()
+            let timeDiff
+            let title, submittedDate, submittedTime
+            let json = []
+            let comment = {}
+            let countIndex = ''
 
+
+            //grab whole html page from given url 
+            let $ = cheerio.load(html)
+            // set parent comments element as data object 
             $('.thing').filter(function () {
-                var data = $(this)
-
+                let data = $(this)
+                //assign wanted comment elements to variable 
                 title = data.find('.entry').find('.title').find('.may-blank').text()
+                submittedTime = data.find('.entry').find('.top-matter').find('.tagline').find('time').attr('datetime')
+                submittedDate = data.find('.entry').find('.top-matter').find('.tagline').find('time').text()
+                //set current time for time elapsed calulation(only get comments less than 24hrs old)
+                timeDiff = (currentdate - new Date(submittedTime)) / 3600000
 
-                submittedTimed = data.find('.entry').find('.top-matter').find('.tagline').find('time').attr('datetime')
-                submittedDated = data.find('.entry').find('.top-matter').find('.tagline').find('time').text()
-                timeDiff = (currentdate - new Date(submittedTimed)) / 3600000
                 if (timeDiff < 24) {
-
+                    //set index count and object key name
                     count++
                     countIndex = 'Comment' + count
+                    //build object for every comment found and push to array
                     comment = {
                         [countIndex]: {
                             title: data.find('.entry').find('.title').find('.may-blank').text(),
                             link: data.find('.entry').find('.top-matter').find('.title').children().first().attr('href'),
-                            submittedTime: submittedTimed,
-                            submittedDate: submittedDated
+                            submittedTime: submittedTime,
+                            submittedDate: submittedDate
                         }
                     }
                     json.push(comment)
@@ -46,6 +51,7 @@ function _getThemComments(url) {
                 }
 
             })
+            //strigify json array, write it to new file, and let the user know
             fs.writeFile(`Comments ${currentdate.toLocaleDateString()}.json`, JSON.stringify(json, null, 4), function (err) {
 
                 console.log('File successfully written! - Check your project directory for the output.json file');
--- server.js
+++ server.js
@@ -1,24 +1,22 @@
-const express = require('express'),
-    readline = require('readline'),
-    comments = require('./getComments'),
-    app = express(),
-    colors = require('colors'),
-    port = 8081
+const
+  readline = require('readline'),
+  comments = require('./getComments'),
+  colors = require('colors'),
+  port = 8081
 
-    app.listen(port)
-    console.log(`The Magic Happens at ${port}`.rainbow)
-    
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout
-    });
-    
-    rl.question('What Reddit URL would you like to scrape for recent comments today? ', (answer) => {
- 
-      comments.getThemComments(answer)
-   
-    
-      rl.close();
-    });
+
+//prompt the user for url and pass url to get comments function 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('What Reddit URL would you like to scrape for recent comments today? ', (answer) => {
+
+  comments.getThemComments(answer)
+
+
+  rl.close();
+});
 
 
