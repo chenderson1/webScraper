31667a498eb844d21f87726de5aadcea6e7aaf73
Cory Henderson <30273325+chenderson1@users.noreply.github.com>
2017-12-13 19:31:51 -0800

    write file of indexed coments working

--- server.js
+++ server.js
@@ -6,35 +6,54 @@ const express = require('express'),
     colors = require('colors'),
     port = 8081
 
+var url = 'https://www.reddit.com/r/warriors/'
 
+return request(url, (error, response, html) => {
+    if (!error) {
+        var count = 0
+        var currentdate = new Date()
+        var timeDiff
+        var $ = cheerio.load(html)
 
+        var title, submittedDate, submittedTimed
+        var json = []
+        var comment = {}
+        var countIndex = ''
 
-    var url = 'https://www.reddit.com/r/warriors/'
+        $('.thing').filter(function () {
+            var data = $(this)
 
-    request(url, (error, response, html)=>{
-        if(!error){
-            var currentdate = new Date()
-            var timeDiff
-            var $ = cheerio.load(html)
+            title = data.find('.entry').find('.title').find('.may-blank').text()
 
-            var title, submittedDate
-            var json = {title: '', submittedDate:''}
+            submittedTimed = data.find('.entry').find('.top-matter').find('.tagline').find('time').attr('datetime')
+            submittedDated = data.find('.entry').find('.top-matter').find('.tagline').find('time').text()
+            timeDiff = (currentdate - new Date(submittedTimed)) / 3600000
+            if (timeDiff < 24) {
 
-            $('.thing').filter(function(){
-                var data = $(this)
-
-                json.title = data.find('.entry').find('.title').find('.may-blank').text()
-                json.submittedDate = data.find('.entry').find('.top-matter').find('.tagline').find('time').attr('datetime')
-                
-                timeDiff= (currentdate - new Date(json.submittedDate)) /3600000
-                if(timeDiff < 24){
-                console.log(json.title)
-                console.log(json.submittedDate)
+                count++
+                countIndex = 'Comment' +count
+                comment = {
+                    [countIndex]: {
+                        title: data.find('.entry').find('.title').find('.may-blank').text(),
+                        link: data.find('.entry').find('.top-matter').find('.title').children().first().attr('href'),
+                        submittedTime: submittedTimed,
+                        submittedDate: submittedDated
+                    }
                 }
-            })
-            
-        }
-    })
+                json.push(comment)
+
+
+            }
+
+        })
+        fs.writeFile('output.json', JSON.stringify(json, null, 4), function (err) {
+
+            console.log('File successfully written! - Check your project directory for the output.json file');
+
+        })
+
+    }
+})
 
 app.listen(port)
 console.log(`The Magic Happens at ${port}`.rainbow)
