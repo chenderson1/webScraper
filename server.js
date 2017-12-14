7c80b049562fd2af219f0de617b1051d0c0ffb2e
Cory Henderson <30273325+chenderson1@users.noreply.github.com>
2017-12-13 19:45:04 -0800

    named and dated ouput file

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ return request(url, (error, response, html) => {
             }
 
         })
-        fs.writeFile('output.json', JSON.stringify(json, null, 4), function (err) {
+        fs.writeFile(`Comments ${currentdate.toLocaleDateString()}.json`, JSON.stringify(json, null, 4), function (err) {
 
             console.log('File successfully written! - Check your project directory for the output.json file');
 
