5523d81282d1fd88b9076ba0dd418a8f93a46e47
Cory Henderson <30273325+chenderson1@users.noreply.github.com>
2017-12-13 17:45:01 -0800

    part 1 working

--- server.js
+++ server.js
@@ -1,17 +1,40 @@
 const express = require('express'),
-fs  = require('fs'),
-request = require('request'),
-cherrio = require('cheerio'),
-app = express(),
-colors = require('colors')
-port = 8081
+    fs = require('fs'),
+    request = require('request'),
+    cheerio = require('cheerio'),
+    app = express(),
+    colors = require('colors'),
+    port = 8081
 
 
 
 
-app.get('scrape', function (req, res){
+    var url = 'https://www.reddit.com/r/warriors/'
 
-})
+    request(url, (error, response, html)=>{
+        if(!error){
+            var currentdate = new Date()
+            var timeDiff
+            var $ = cheerio.load(html)
+
+            var title, submittedDate
+            var json = {title: '', submittedDate:''}
+
+            $('.thing').filter(function(){
+                var data = $(this)
+
+                json.title = data.find('.entry').find('.title').find('.may-blank').text()
+                json.submittedDate = data.find('.entry').find('.top-matter').find('.tagline').find('time').attr('datetime')
+                
+                timeDiff= (currentdate - new Date(json.submittedDate)) /3600000
+                if(timeDiff < 24){
+                console.log(json.title)
+                console.log(json.submittedDate)
+                }
+            })
+            
+        }
+    })
 
 app.listen(port)
 console.log(`The Magic Happens at ${port}`.rainbow)
